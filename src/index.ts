acce9b78a66959d394ccad4b5d11b48c928e5603
Anderson Suga <anderson.suga@gmail.com>
2024-01-15 15:41:04 -0300

    Route and Handler

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,24 @@ import { Elysia } from "elysia";
 
 const app = new Elysia().get("/", () => "Hello Elysia")
 .get("/post/:id",({params: {id}}) => {return {id: id, title: "Learn Bun"}})
+.post("/post", ({body ,set}) => { 
+  set.status = 201;
+  return body;
+})
+.get("/track/*", () => {return 'Track Route'})
+.get("/tracks", () => {
+  return new Response(JSON.stringify({
+    "tracks" : [
+      'Dancing Feat',
+      'Sam I',
+      'Animals'
+    ]
+  }), {
+    headers:  {
+      'Content-Type': 'application/json'
+    }
+  })
+})
 .listen(3000);
 
 console.log(
